a979d4634c3363195c784592a53387c36599ff5c
Rob Young <rhyoung214@gmail.com>
2023-07-30 15:14:41 -0700

    setup styling

--- app/javascript/Dog.js
+++ app/javascript/Dog.js
@@ -1,1 +1,18 @@
-import { dogs } from 'data'
+// import { dogs } from 'data'
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data)
+  }
+
+  getDogHtml() {
+    const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this
+    return `
+      <img class="avatar" src="/assets/${avatar}">
+      <p class="dog-info">${name}, ${age}</p>
+      <p class="dog-bio">${bio}</p>
+    `
+  }
+}
+
+export default Dog
