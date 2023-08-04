aa2cd2bf995834a700999d3b79acd72025b097fa
Rob Young <rhyoung214@gmail.com>
2023-08-03 17:37:17 -0700

    updated fnewDog functionality

--- app/javascript/Dog.js
+++ app/javascript/Dog.js
@@ -7,11 +7,28 @@ class Dog {
 
   getDogHtml() {
     const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this
-    return `
-      <img class="avatar" src="/assets/${avatar}">
+    let dogPostHtml = ''
+
+    if (hasBeenSwiped) {
+      if (hasBeenLiked) {
+        dogPostHtml = `<img class="badge like-dog" src="/assets/badge-like.png">`
+      } else {
+        dogPostHtml = `<img class="badge reject-dog" src="/assets/badge-nope.png">`
+      }
+    }
+    return dogPostHtml += `
+    <img class="avatar" src="/assets/${avatar}">
       <p class="dog-info">${name}, ${age}</p>
       <p class="dog-bio">${bio}</p>
     `
+  }
+
+  updateHasBeenSwiped() {
+    this.hasBeenSwiped = true
+  }
+
+  toggleLikeStatus() {
+    this.hasBeenLiked = !this.hasBeenLiked
   }
 }
 
--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,6 +1,8 @@
 import dogs  from 'data'
 import Dog from 'Dog'
 
+let isWaiting = false
+
 const getDogNamesArray = function() {
   return dogs.map(dog => dog.name)
 }
@@ -10,7 +12,31 @@ function getNewDog() {
   return nextDog ? new Dog(nextDog) : {}
 }
 
-// const dogNamesArray = getDogNamesArray()
-const newDog = getNewDog()
+function likeDog() {
+  newDog.hasBeenLiked = true
+  render()
+  setTimeout(() => {
+    newDog = getNewDog()
+    render()
+  }, 2000)
+}
 
-document.getElementById('dog-post').innerHTML = newDog.getDogHtml()
+function rejectDog() {
+  newDog.hasBeenLiked = false
+  render()
+  setTimeout(() => {
+    newDog = getNewDog()
+    render()
+  }, 2000)
+}
+
+document.getElementById("like-dog").addEventListener('click', likeDog)
+document.getElementById("reject-dog").addEventListener('click', rejectDog)
+
+function render() {
+  document.getElementById('dog-post').innerHTML = newDog.getDogHtml()
+  newDog.updateHasBeenSwiped()
+}
+
+let newDog = getNewDog()
+render()
